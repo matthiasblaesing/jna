3416061e7a407dd0aeab3bc1a21b9d5644a2c395
Matthias Bläsing <mblaesing@doppel-helix.eu>
2019-03-20 20:35:57 +0100

    [GITHUB-1076] Test java boolean mapping to C boolean (1 byte)

---
+++ test/com/sun/jna/BooleanMappingTest.java
@@ -0,0 +1,131 @@
+/* Copyright (c) 2019 Matthias Bläsing, All Rights Reserved
+ *
+ * The contents of this file is dual-licensed under 2
+ * alternative Open Source/Free licenses: LGPL 2.1 or later and
+ * Apache License 2.0. (starting with JNA version 4.0.0).
+ *
+ * You can freely decide which license you want to apply to
+ * the project.
+ *
+ * You may obtain a copy of the LGPL License at:
+ *
+ * http://www.gnu.org/licenses/licenses.html
+ *
+ * A copy is also included in the downloadable source code package
+ * containing JNA, in file "LGPL2.1".
+ *
+ * You may obtain a copy of the Apache License at:
+ *
+ * http://www.apache.org/licenses/
+ *
+ * A copy is also included in the downloadable source code package
+ * containing JNA, in file "AL2.0".
+ */
+
+package com.sun.jna;
+
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BooleanMappingTest {
+    public static interface TestLibrary extends Library {
+        public TestLibrary INSTANCE = Native.load("testlib", TestLibrary.class, getOptions());
+        // It is always the native function returnInt8Argument that is called
+        // in getOptions a corresponding typemapper is added to the library options
+        byte booleanPrimitiveToByte(boolean arg);
+        byte booleanObjectToByte(Boolean arg);
+        boolean byteToBooleanPrimitive(byte arg);
+        Boolean byteToBooleanObject(byte arg);
+    }
+
+    public static class TestLibraryDirect {
+        static {
+            Native.register(NativeLibrary.getInstance("testlib", getOptions()));
+        }
+
+        public static final TestLibraryDirect INSTANCE = new TestLibraryDirect();
+
+        public native byte booleanPrimitiveToByte(boolean arg);
+        public native byte booleanObjectToByte(Boolean arg);
+        public native boolean byteToBooleanPrimitive(byte arg);
+        public native Boolean byteToBooleanObject(byte arg);
+    }
+
+    @Test
+    public void testMarshallingToInt8() {
+        assertEquals(0, TestLibrary.INSTANCE.booleanPrimitiveToByte(false));
+        assertEquals((byte) 0xFF, TestLibrary.INSTANCE.booleanPrimitiveToByte(true));
+        assertEquals(0, TestLibrary.INSTANCE.booleanObjectToByte(Boolean.FALSE));
+        assertEquals((byte) 0xFF, TestLibrary.INSTANCE.booleanObjectToByte(Boolean.TRUE));
+    }
+
+    @Test
+    public void testUnmarshallingFromInt8() {
+        assertFalse(TestLibrary.INSTANCE.byteToBooleanPrimitive((byte) 0));
+        assertTrue(TestLibrary.INSTANCE.byteToBooleanPrimitive((byte) 1));
+        assertTrue(TestLibrary.INSTANCE.byteToBooleanPrimitive((byte) 0xFF));
+        assertFalse(TestLibrary.INSTANCE.byteToBooleanObject((byte) 0));
+        assertTrue(TestLibrary.INSTANCE.byteToBooleanObject((byte) 1));
+        assertTrue(TestLibrary.INSTANCE.byteToBooleanObject((byte) 0xFF));
+    }
+
+    @Test
+    public void testMarshallingToInt8Direct() {
+        assertEquals(0, TestLibraryDirect.INSTANCE.booleanPrimitiveToByte(false));
+        assertEquals((byte) 0xFF, TestLibraryDirect.INSTANCE.booleanPrimitiveToByte(true));
+        assertEquals(0, TestLibraryDirect.INSTANCE.booleanObjectToByte(Boolean.FALSE));
+        assertEquals((byte) 0xFF, TestLibraryDirect.INSTANCE.booleanObjectToByte(Boolean.TRUE));
+    }
+
+    @Test
+    public void testUnmarshallingFromInt8Direct() {
+        assertFalse(TestLibraryDirect.INSTANCE.byteToBooleanPrimitive((byte) 0));
+        assertTrue(TestLibraryDirect.INSTANCE.byteToBooleanPrimitive((byte) 1));
+        assertTrue(TestLibraryDirect.INSTANCE.byteToBooleanPrimitive((byte) 0xFF));
+        assertFalse(TestLibraryDirect.INSTANCE.byteToBooleanObject((byte) 0));
+        assertTrue(TestLibraryDirect.INSTANCE.byteToBooleanObject((byte) 1));
+        assertTrue(TestLibraryDirect.INSTANCE.byteToBooleanObject((byte) 0xFF));
+    }
+
+    private static Map<String,?> getOptions() {
+        Map<String,Object> result = new HashMap<>();
+        // We want to call the same native function under different names
+        result.put(Library.OPTION_FUNCTION_MAPPER, new FunctionMapper() {
+            @Override
+            public String getFunctionName(NativeLibrary library, Method method) {
+                return "returnInt8Argument";
+            }
+        });
+        DefaultTypeMapper typeMapper = new DefaultTypeMapper();
+
+        typeMapper.addTypeConverter(Boolean.class, new TypeConverter() {
+            @Override
+            public Object fromNative(Object nativeValue, FromNativeContext context) {
+                return ((byte) nativeValue) != 0;
+            }
+
+            @Override
+            public Class<?> nativeType() {
+                return byte.class;
+            }
+
+            @Override
+            public Object toNative(Object value, ToNativeContext context) {
+                if(value.getClass() == Boolean.class) {
+                    value = (boolean) value;
+                }
+                if((boolean) value) {
+                    return (byte) 0xFF;
+                } else {
+                    return 0;
+                }
+            }
+        });
+
+        result.put(Library.OPTION_TYPE_MAPPER, typeMapper);
+        return result;
+    }
+}
